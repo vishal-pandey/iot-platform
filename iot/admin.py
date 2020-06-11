dcaf70f634f151e74d18a9005d16b753fdc6138e
VISHAL PANDEY <contact@vishalpandey.xyz>
2020-06-11 21:44:18 +0530

    MySql Update and ACL

--- iot/admin.py
+++ iot/admin.py
@@ -1,33 +1,73 @@
 from django.contrib import admin
-from iot.models import iotApp
+from iot.models import iotApp, device
 import uuid
 import os
+from django.contrib.auth.hashers import make_password
+from pprint import pprint
+from inspect import getmembers
+
+class deviceAdmin(admin.TabularInline):
+	model = device
+	fieldsets = ((None, {'fields': ('name',)}), )
+
 
 class iotAppAdmin(admin.ModelAdmin):
 	model = iotApp
 	list_display = ['name', 'key']
+	# list_display = ['name', 'key', 'username', 'password', 'pw']
 	fieldsets = ((None, {'fields': ('name',)}), )
+	inlines = [deviceAdmin]
+
 
 	def save_model(self, request, obj, form, change):
-		obj.key = uuid.uuid4()
-		obj.owner = request.user
+		if not change:
+			obj.key = uuid.uuid4()
+			obj.owner = request.user
 
-		username = uuid.uuid4()
-		obj.username = username
+			username = uuid.uuid4()
+			obj.username = username
 
-		password = uuid.uuid4()
-		obj.password = password
+			self.username = username
 
-		command = "sudo mosquitto_passwd -b /etc/mosquitto/passwd "+str(username)+""+" "+str(password)+""
-		os.system(command)
-		os.system("sudo kill -HUP 18371")
+			password = uuid.uuid4()
+			obj.password = password
+
+			obj.pw = make_password(password)
+		if change:
+			self.username = obj.username
+
+		# command = "sudo mosquitto_passwd -b /etc/mosquitto/passwd "+str(username)+""+" "+str(password)+""
+		# os.system(command)
+		# os.system("sudo kill -HUP 18371")
 
 		obj.save()
+
+
+	def save_formset(self, request, form, formset, change):
+		formset.save()
+		
+		for f in formset.forms:
+			obj = f.instance
+			# print(obj.name) 
+			obj.topic = str(self.username)+"/"+obj.name+"/#"
+			# pprint(getmembers(obj))
+			if str(obj.name) != "":
+				obj.save()
+
+
+
+	# def save_related(self, request, obj, form, change):
+	# 	username = form
+	# 	print(username)
+	# 	print("Hello World")
+	# 	# obj.topic = username+"/"+name
+	# 	obj.save()
+
 
 	def get_queryset(self, request):
 		qs = super(iotAppAdmin, self).get_queryset(request)
 		if request.user.is_superuser:
-			self.list_display = ['name', 'key', 'username', 'password']
+			self.list_display = ['name', 'key', 'username', 'password', 'pw']
 			return qs
 		else:
 			return qs.filter(owner = request.user)
--- iot/models.py
+++ iot/models.py
@@ -6,8 +6,18 @@ from django.contrib.auth.models import User
 class iotApp(models.Model):
 	name = models.CharField(blank=False, max_length=255)
 	key = models.CharField(blank=False, max_length=255)
-	owner = models.ForeignKey(User, on_delete=models.CASCADE)
-	username = models.CharField(blank=True, max_length=255, null=True)
-	password = models.CharField(blank=True, max_length=255, null=True)	
+	owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+	username = models.CharField(blank=True, max_length=255, null=True, unique=True)
+	password = models.CharField(blank=True, max_length=255, null=True)
+	pw = models.CharField(blank=True, max_length=128, null=True)
+	super = models.SmallIntegerField(default=0)
+	def __str__(self):
+		return self.name
+
+class device(models.Model):
+	username = models.ForeignKey(iotApp, to_field='username',on_delete=models.SET_NULL, db_column='username', null=True, blank=True)
+	name = models.CharField(blank=False, max_length=255)
+	topic = models.CharField(blank=False, max_length=255)
+	rw = models.SmallIntegerField(default=1)
 	def __str__(self):
 		return self.name
